46a4b12b431f7d881d68f6161029cf7954390b2a
Khadija-Aziz <khadijaaziz670@gmail.com>
2026-03-14 16:06:38 +0000

    intial spring boot projet

---
+++ src/main/java/com/example/suivi_projet/organisation/entities/Profil.java
@@ -0,0 +1,49 @@
+package com.example.suivi_projet.organisation.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Profil {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String code;
+    private String libelle;
+
+    public Profil() {
+    }
+
+    public Profil(int id, String code, String libelle) {
+        this.id = id;
+        this.code = code;
+        this.libelle = libelle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+}
