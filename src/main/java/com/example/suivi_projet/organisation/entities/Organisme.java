85b4b3cd9c1f50b00b6e3785e14c628af5322a2c
Khadija-Aziz <khadijaaziz670@gmail.com>
2026-03-14 16:39:59 +0000

    add Organisme.java

---
+++ src/main/java/com/example/suivi_projet/organisation/entities/Organisme.java
@@ -0,0 +1,90 @@
+package com.example.suivi_projet.organisation.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Organisme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String code;
+    private String nom;
+    private String adresse;
+    private String telephone;
+    private String nomContact;
+    private String emailContact;
+    private String siteWeb;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+
+    public String getNomContact() {
+        return nomContact;
+    }
+
+    public void setNomContact(String nomContact) {
+        this.nomContact = nomContact;
+    }
+
+
+    public String getEmailContact() {
+        return emailContact;
+    }
+
+    public void setEmailContact(String emailContact) {
+        this.emailContact = emailContact;
+    }
+
+
+    public String getSiteWeb() {
+        return siteWeb;
+    }
+
+    public void setSiteWeb(String siteWeb) {
+        this.siteWeb = siteWeb;
+    }
+}
