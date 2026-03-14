63650d38730d77c6f714cbad44e014b7f22fc3ca
najiazad20 <najiazad2000@gmail.com>
2026-03-14 23:04:52 +0100

    update ProfilRepository

--- src/main/java/com/example/suivi_projet/organisation/repositories/ProfilRepository.java
+++ src/main/java/com/example/suivi_projet/organisation/repositories/ProfilRepository.java
@@ -1,7 +1,10 @@
 package com.example.suivi_projet.organisation.repositories;
 
+import com.example.suivi_projet.organisation.entities.Organisme;
 import com.example.suivi_projet.organisation.entities.Profil;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfilRepository extends JpaRepository<Profil, Integer> {
+
+    Profil findByCode(String code);
 }
